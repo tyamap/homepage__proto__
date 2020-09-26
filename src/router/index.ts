01f95df885b6c9318764f34c206721a01e009ab5
tyamap <tom333my@outlook.com>
2020-09-26 17:02:28 +0900

    カルーセルクリックでハッシュIDにジャンプ

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,13 +14,38 @@ const routes: Array<RouteConfig> = [
     path: '/about',
     name: 'About',
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/works',
+    name: 'Works',
+    component: () => import(/* webpackChunkName: "works" */ '../views/Works.vue')
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: () => import(/* webpackChunkName: "blog" */ '../views/Blog.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import(/* webpackChunkName: "contact" */ '../views/Contact.vue')
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return {
+        selector: to.hash
+      };
+    }
+  }
 })
 
 export default router
